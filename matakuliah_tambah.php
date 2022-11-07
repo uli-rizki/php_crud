667376204863da0b70e5ad75dd64a9a008ff41d9
uli <ulirizki@gmail.com>
2022-11-07 11:38:08 +0700

    form tambah matakuliah

---
+++ matakuliah_tambah.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Jurnal UNUHA</title>
+    </head>
+    <body>
+        <h1>Tambah Matakuliah</h1>
+
+        <form action="" method="POST">
+            <label>Nama Matakuliah</label>
+            <input type="text" id="nama_matakuliah" name="nama_matakuliah" required>
+            <br />
+            <label>Bobot</label>
+            <input type="text" id="bobot" name="bobot" required>
+            <br />
+            <label>Semester</label>
+            <input type="text" id="semester" name="semester" required>
+            <br />
+            <input type="submit" name="submit" value="Simpan">
+        </form>
+
+        <br />
+        <a href="matakuliah_tampil.php">Kembali Ke Daftar Makul</a>
+    </body>
+</html>
